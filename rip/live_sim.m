c95981fa99ac45fa41f874aa751e99eb4181dc7f
GaetanoPaolo <gaetcondition0@gmail.com>
2022-05-03 15:07:10 +0200

    live_simulation_file

---
+++ rip/live_sim.m
@@ -0,0 +1,27 @@
+%% Loading parameters
+trial_index = 1;
+fprintf("\nINFO: starting trial %d\n", trial_index);
+Q = trial_LUT(trial_index).Q;
+R = trial_LUT(trial_index).R;
+[K_control,~,~] = lqr(sys, Q, R); % K_control is fed into the simulink diagram
+experiment_scenario = "disturbanceRejection"; % choices are "setPointTracking" or "disturbanceRejection"
+%Simulink parameters
+% simulink diagram related parameters
+wc = 2*2*pi; % parameter required to calculate alpha and theta filter, given in the assignment.
+K = 1; % parameter required to calculate alpha and theta filter, given in the assignment.
+Ts = 1 / fs;
+
+x0 = [0, 0, 0, 0]; xd = [0, 0, 0, 0]; 
+% initial and desired state of the system should be updated after the experiment scenario is set.
+if experiment_scenario == "setPointTracking"  % define when the setpoint is changed and the amplitude of the change
+  setpoint_theta= 90; % in degree, not in radian.
+  x0 = x0; % initial state not changed in set point tracking, still zero * 4.
+  xd = [setpoint_theta*pi/180, 0, 0, 0]; % degree to radian conversion
+elseif experiment_scenario == "disturbanceRejection"
+  xd = xd; % desired state not changed in disturbance rejection, still zero * 4.
+else
+  error("Unknown experiment scenario");
+end
+
+%% Saving simout
+save('sim1.mat','simout');
